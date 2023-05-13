d4b477a8fcd25f997259db758b4f31ab0ab28dd1
LuoLuann <luo.luann@gmail.com>
2023-05-13 00:21:31 -0300

    install and config vue-router

--- backend/src/main.js
+++ backend/src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 
-import store from './store/store'
+import store from '../src/store/store'
+import router from './router';
+
 //import './style.css'
 import './index.css'
 import App from './App.vue'
 
 createApp(App)
     .use(store)
+    .use(router)
     .mount('#app')
---
+++ backend/src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Login form</h1>
+    aaaaaaaaaaaaa
+</template>
+
+<script>
+    export default {
+        name: 'Login',
+    }
+</script>
+
+<style scoped>
+
+</style>
