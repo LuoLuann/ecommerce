4f10eafc6e37ef62a7cd001b77857d2323be3dd9
LuoLuann <luo.luann@gmail.com>
2023-05-12 23:16:59 -0300

    add store js

---
+++ backend/src/store/store.js
@@ -0,0 +1,13 @@
+ import {createStore} from "vuex";
+
+ const store = createStore({
+   //an object of objects
+    state: {
+      test: '1234567',
+    },
+    getters: {},
+    actions: {},
+    mutations: {},
+ });
+
+ export default store
