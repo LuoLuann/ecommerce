a174d23019ffec40885866a1f1b33b8cd63d3e7d
LuoLuann <luo.luann@gmail.com>
2023-05-15 01:56:04 -0300

    form logi, password request and password reset makes with tailwind css templates

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Dashboard from '../views/Dashboard.vue';
 import Login from '../views/Login.vue';
+import RequestPassword from "../views/RequestPassword.vue"
+import ResetPassword from "../views/ResetPassword.vue"
+
 
 const routes = [
   {
@@ -12,7 +15,26 @@ const routes = [
     path: '/login',
     name: 'login',
     component: Login,
+    meta: {
+      requiresGuest: true
+    }
   },
+  {
+    path: '/request-password',
+    name: 'requestPassword',
+    component: RequestPassword,
+    meta: {
+      requiresGuest: true
+    }
+  },
+  {
+    path: '/reset-password/:token',
+    name: 'resetPassword',
+    component: ResetPassword,
+    meta: {
+      requiresGuest: true
+    }
+  }
 ];
 
 const router = createRouter({
